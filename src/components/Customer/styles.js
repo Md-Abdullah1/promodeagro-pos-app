6fcc526daae9b543efc8464be3dc36de93c2860e
Md-Abdullah1 <abdullahahil153@gmail.com>
2024-04-27 03:07:31 +0530

    working on header section of customers

--- src/components/Customer/styles.js
+++ src/components/Customer/styles.js
@@ -8,5 +8,42 @@ export default StyleSheet.create({
     bgPrimary: {
       backgroundColor: '#FC8019',
     },
+    textSecondary: {
+      color: '#09AA29',
+    },
+    bgSecondary: {
+      backgroundColor: '#09AA29',
+    },
+    textTertiary: {
+      color: '#FFF2E8',
+    },
+    bgTertiary: {
+      backgroundColor: '#FFF2E8',
+    },
+    textBlack: {
+      color: '#171826',
+    },
+    bgBlack: {
+      backgroundColor: '#171826',
+    },
+    textGrey: {
+      color: '#9F9F9E',
+    },
+    bgGrey: {
+      backgroundColor: '#9F9F9E',
+    },
+    textWhite: {
+      color: '#F5F5F5',
+    },
+    bgWhite: {
+      backgroundColor: '#F5F5F5',
+    },
+    header:{
+      height:'10%',
+      width:'100%',
+    },
+    table:{
+      width:'100%'
+    }
   
 })
